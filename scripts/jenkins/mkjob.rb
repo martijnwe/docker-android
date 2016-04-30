ff2301c7c151598d77544cbf9309c841b4350d2b
martijnwe <martijnwe@gmail.com>
2016-04-30 17:20:11 +0000

    modified:   scripts/jenkins/mkjob.rb 	scripts/jenkins/config.xml

--- scripts/jenkins/mkjob.rb
+++ scripts/jenkins/mkjob.rb
@@ -1,6 +1,6 @@
 require 'jenkins_api_client'
 
-@client = JenkinsApi::Client.new(:server_ip => 'localhost')
+@client = JenkinsApi::Client.new(:server_ip => 'localhost',:username => 'jenkins', :password => 'jenkins')
 # The following call will return all jobs matching 'Testjob'
 puts @client.job.list_all
 
