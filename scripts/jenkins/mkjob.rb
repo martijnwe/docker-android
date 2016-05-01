7a2b735926f0a14e215806b8aa0d19042f99af01
martijnwe <martijnwe@gmail.com>
2016-05-01 18:27:47 +0000

    modified:   environments.yml 	modified:   mkjob.rb

--- scripts/jenkins/mkjob.rb
+++ scripts/jenkins/mkjob.rb
@@ -19,30 +19,48 @@ end
 
 @client.system.wait_for_ready
 
-descriptor['jobs'].each {|k,job| 
+descriptor['jobs'].each do |jobdesc| 
+    job = jobdesc['job']
+
+    if @client.job.exists?(job['name'])
+       @client.job.delete(job['name'])
+    end
+
+
+    params = {}
+    params[:name] = job['name'] 
+    params[:keep_dependencies] = true 
+    params[:concurrent_build] = true 
+    params[:scm_provider] = job['vcs_provider'] 
+    params[:scm_url] = job['vcs_url'] 
+    params[:shell_command] = job['buildcmd'] 
+    
+    if job['schedule']
+        params[:timer] = job['schedule'] 
+    end
+   
+    if job['children']
+        params[:child_projects] = job['children'] 
+    end 
 
     # Create job
-    @client.job.create_freestyle(
-      :name => job['name'],
-      :keep_dependencies => true,
-      :concurrent_build => true,
-      :scm_provider => job['vcs_provider'],
-      :scm_url => job['vcs_url'],
-      :shell_command => job['buildcmd'])
+    @client.job.create_freestyle(params)
 
     # Configure properties
-    xml = @client.job.get_config(job['name'])
-    n_xml = Nokogiri::XML(xml)
-    p_xml = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |b_xml|
-       b_xml.send("hudson.plugins.envfile.EnvFileBuildWrapper") {
-           b_xml.filePath job['properties']
-       }
+    if job['properties'] 
+        xml = @client.job.get_config(job['name'])
+        n_xml = Nokogiri::XML(xml)
+        p_xml = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |b_xml|
+           b_xml.send("hudson.plugins.envfile.EnvFileBuildWrapper") {
+               b_xml.filePath job['properties']
+           }
+        end
+        envFileWrapperXml = Nokogiri::XML(p_xml.to_xml).xpath(
+           "//hudson.plugins.envfile.EnvFileBuildWrapper"
+        ).first
+        n_xml.xpath("//buildWrappers").first.add_child(envFileWrapperXml)
+        @client.post_config("/job/#{path_encode job['name']}/config.xml", n_xml.to_xml)
     end
-    envFileWrapperXml = Nokogiri::XML(p_xml.to_xml).xpath(
-       "//hudson.plugins.envfile.EnvFileBuildWrapper"
-    ).first
-    n_xml.xpath("//buildWrappers").first.add_child(envFileWrapperXml)
-    @client.post_config("/job/#{path_encode job['name']}/config.xml", n_xml.to_xml)
-}
+end
 
 
